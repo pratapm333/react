3ab0cac6aa52a00687c66bc84fd4aab7d4c70a8d
Pratapm333 <pratap3090@gmail.com>
2024-09-04 13:12:56 +0530

    SignIn/SignUp and Login/Header

--- netflix-gpt/src/components/Body.js
+++ netflix-gpt/src/components/Body.js
@@ -1,12 +1,25 @@
+import {createBrowserRouter} from "react-router-dom"; 
 import React from 'react'
 import Login from './Login'
 import Browse from './Browse'
+import { RouterProvider } from "react-router-dom";
 
 const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login />,
+        },
+        {
+            path: "/browse",
+            element: <Browse />,
+        },
+    ]);
+
   return (
     <div>
-        <Login />
-        <Browse />
+        <RouterProvider router={appRouter} />
     </div>
   )
 };
--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Header from './Header';
 
 const Login = () => {
+
+  const [isSignInForm, setIsSignInForm] = useState(true)
+
+  const toggleSignInForm = () => {
+    setIsSignInForm( !isSignInForm)
+  }
+   
   return (
-    <div>Login</div>
+
+   
+    <div>
+        <Header />
+        <div className='absolute'>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/dae1f45f-c2c5-4a62-8d58-6e1b0c6b2d8e/6d1fb8a4-5844-42a4-9b01-1c6c128acf19/IN-en-20240827-TRIFECTA-perspective_WEB_c292a608-cdc6-4686-8dc8-405bfcf753af_large.jpg" alt="background" />
+        
+        </div>
+
+        <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+          <h1 className='font-bold text-3xl py-4 m-2 '>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+         {!isSignInForm && (
+          <input type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700'/>
+         )} 
+          <input type='email' placeholder='Email' className='p-4 my-4 w-full bg-gray-700'/>
+          <input type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700 '/>
+          <button className='p-4 my-4 bg-red-700 w-full' >Sign In </button>
+          <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>
+            {isSignInForm ? "New to Netflix? Sign up now" : "Already Registered! Sign In now"}
+          
+            </p>
+        </form>
+    </div>
   )
 }
 
