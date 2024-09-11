1db7deaef8c2251ee5ee71f0a970ca6a36f355dd
Pratapm333 <pratap3090@gmail.com>
2024-09-11 12:33:50 +0530

    form validation

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './Header';
+import { checkValidData } from '../utils/validate';
 
 const Login = () => {
 
-  const [isSignInForm, setIsSignInForm] = useState(true)
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage,setErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    console.log(name.current.value);
+    console.log(email.current.value);
+    console.log(password.current.value);
+
+    const message = checkValidData( name.current.value, email.current.value, password.current.value);
+    setErrorMessage(message);
+
+  }
 
   const toggleSignInForm = () => {
     setIsSignInForm( !isSignInForm)
@@ -19,14 +35,16 @@ const Login = () => {
         
         </div>
 
-        <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+        <form onSubmit={(e) => e.preventDefault()} className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
           <h1 className='font-bold text-3xl py-4 m-2 '>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
          {!isSignInForm && (
-          <input type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700'/>
+          <input type='name' ref={name} placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700'/>
          )} 
-          <input type='email' placeholder='Email' className='p-4 my-4 w-full bg-gray-700'/>
-          <input type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700 '/>
-          <button className='p-4 my-4 bg-red-700 w-full' >Sign In </button>
+          <input type='email' ref={email} placeholder='Email' className='p-4 my-4 w-full bg-gray-700'/>
+          <input type='password' ref={password} placeholder='Password' className='p-4 my-4 w-full bg-gray-700 '/>
+          
+          <p className='text-red-500 text-lg font-bold'>{errorMessage}</p>
+          <button className='p-4 my-4 bg-red-700 w-full' onClick={handleButtonClick}>Sign In </button>
           <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>
             {isSignInForm ? "New to Netflix? Sign up now" : "Already Registered! Sign In now"}
           
---
+++ netflix-gpt/src/utils/validate.js
@@ -0,0 +1,12 @@
+export const checkValidData = (name, email, password) => {
+    const isNameValid = /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/.test(name);
+    const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+    
+    if(!isNameValid) return "Full name is not valid";
+    if(!isEmailValid) return "Email id is not valid";
+    if(!isPasswordValid) return "Password is not valid";
+
+    return null;
+
+}
